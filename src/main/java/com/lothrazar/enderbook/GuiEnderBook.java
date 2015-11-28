2ea8cb73f4c00f3a89b333cfba2961f9b05699e1
sam bassett <samson.bassett@gmail.com>
2015-11-27 18:38:01 -0800

    fixing attempts for spawn particle

--- src/main/java/com/lothrazar/enderbook/GuiEnderBook.java
+++ src/main/java/com/lothrazar/enderbook/GuiEnderBook.java
@@ -155,14 +155,21 @@ public class GuiEnderBook  extends GuiScreen
 		this.entityPlayer.closeScreen();
 	}
 	
-	private void particleAtPlayer(World world,EntityPlayer p){
-
-		world.spawnParticle(EnumParticleTypes.PORTAL, p.posX, p.posY, p.posZ 
-				, p.posX + world.rand.nextDouble() * 0.6D 
-				, p.posY + world.rand.nextDouble() * 0.6D 
-				, p.posZ + world.rand.nextDouble() * 0.6D  );
+	private void particleAtPlayer(World world,EntityPlayer p)
+	{ 
+		spawnParticle(world,p.getPosition().getX(),p.getPosition().getY(),p.getPosition().getZ());
+		spawnParticle(world,p.getPosition().getX(),p.getPosition().getY()+1,p.getPosition().getZ());
+		spawnParticle(world,p.getPosition().getX(),p.getPosition().getY()+2,p.getPosition().getZ());
 	}
-	
+	private void spawnParticle(World world,  double x, double y, double z)
+	{ 
+		//http://www.minecraftforge.net/forum/index.php?topic=9744.0
+		for(int countparticles = 0; countparticles <= 12; ++countparticles)
+		{
+			world.spawnParticle(EnumParticleTypes.PORTAL, x + (world.rand.nextDouble() - 0.5D) * (double)0.8, y + world.rand.nextDouble() * (double)1.5 - (double)0.1, z + (world.rand.nextDouble() - 0.5D) * (double)0.8
+					, 0.0D, 0.0D, 0.0D);
+		}
+	}
 	@Override
 	public boolean doesGuiPauseGame()
 	{
